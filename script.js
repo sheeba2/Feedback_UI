5f914651ad4cfe8881442d5b6b219f4da91eabda
Sheeba Sayeed <61542956+sheeba2@users.noreply.github.com>
2024-11-10 02:29:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const ratingEls = document.querySelectorAll(".rating");
+
+const btnEl = document.getElementById("btn");
+
+const containerEl = document.getElementById("container");
+
+let selectedRating = "";
+
+ratingEls.forEach((ratingEl)=>{
+    ratingEl.addEventListener("click",(event)=>{
+        // console.log(event.target.innerText || event.target.parentNode.innerText);
+        removeActive()
+        selectedRating = event.target.innerText || event.target.parentNode.innerText; 
+        event.target.classList.add("active")
+        event.target.parentNode.classList.add("active")
+
+    });
+})
+
+btnEl.addEventListener("click",()=>{
+    if(selectedRating !==""){
+        containerEl.innerHTML=`<strong>Thank You ! </strong><br><br> <strong>Feedback:${selectedRating}</strong><p>
+        We'll Use your feedback to Improve our customer support
+        </p>`
+
+    }
+})
+
+function removeActive(){
+    ratingEls.forEach((ratingEl)=>{
+        ratingEl.classList.remove('active')
+    })
+
+}
